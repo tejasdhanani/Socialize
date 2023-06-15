9654b0aa549bdd0abc2886b4b238d9c08ac5798f
tejasdhanani <tejas27dhanani@gmail.com>
2023-06-15 16:02:45 -0700

    connected database and errorHandler

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
+const connectDB = require("./config/db");
+const { errorHandler } = require("./middleware/errorMiddleware");
+
+connectDB();
 
 const app = express();
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server stared ${port}`));
