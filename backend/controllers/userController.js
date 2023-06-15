73bede6e6b7da63c85bc2075d8baa752761a3902
tejasdhanani <tejas27dhanani@gmail.com>
2023-06-15 16:47:22 -0700

    login user and generate JWT token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,6 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -53,6 +54,23 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route      POST  /api/users/login
 // @access     Public
 const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  // check for user email
+  const user = await User.findOne({ email });
+
+  if (user && bcrypt.compare(password, user.password)) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Could not login the user");
+  }
+
   res.json({
     message: "Login user",
   });
@@ -60,12 +78,19 @@ const loginUser = asyncHandler(async (req, res) => {
 
 // @desc       Get User Data
 // @route      GET  /api/users/me
-// @access     Public
+// @access     Private
 const getMe = asyncHandler(async (req, res) => {
   res.json({
     message: "Get user",
   });
 });
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = {
   registerUser,
